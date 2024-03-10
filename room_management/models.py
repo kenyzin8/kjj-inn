b4f1d54ef1d77088021e60cda330532cbbadc6b7
Kent <jamila.211148@koronadal.sti.edu.ph>
2024-03-10 21:29:31 +0800

    Initial commit

---
+++ room_management/models.py
@@ -0,0 +1,50 @@
+from django.db import models
+
+class Building(models.Model):
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+class Fee(models.Model):
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    hours = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.amount}"
+
+class Room(models.Model):
+    room_number = models.IntegerField()
+    building = models.ForeignKey(Building, on_delete=models.CASCADE)
+    fee = models.ForeignKey(Fee, on_delete=models.CASCADE)
+    room_type = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.building.name} - {self.room_number}"
+
+class Customer(models.Model):
+    alias = models.CharField(max_length=100, default="Anonymous")
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    check_in_date = models.DateTimeField()
+    check_out_date = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    amount_paid = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.room.building.name} - {self.room.room_number} - {self.check_in_date} - {self.check_out_date}"
