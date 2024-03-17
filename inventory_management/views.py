91821860db5c1addd2db6e132a0805b3b67921b4
Kent <jamila.211148@koronadal.sti.edu.ph>
2024-03-17 21:18:37 +0800

    update

--- inventory_management/views.py
+++ inventory_management/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
 from .models import *
+from room_management.models import Room
 
 def store(request):
     products = Product.objects.all()
     stocks = Stock.objects.all()
+    active_rooms = Room.objects.filter(is_active=True)
 
     context = {
         'products': products,
-        'stocks': stocks
+        'stocks': stocks,
+        'active_rooms': active_rooms
     }
 
     return render(request, 'store.html', context)
