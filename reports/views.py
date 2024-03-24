2d6234e07582047b923ec2755a454ee8dd055d78
Kent <jamila.211148@koronadal.sti.edu.ph>
2024-03-24 20:32:20 +0800

    paganation

--- reports/views.py
+++ reports/views.py
@@ -3,6 +3,8 @@ from inventory_management.models import Purchase, PurchaseItem
 from room_management.models import Customer, ExtraBedPrice
 from django.contrib.auth.decorators import login_required
 from core.decorators import staff_required
+from django.core.paginator import Paginator
+from django.core.paginator import EmptyPage, PageNotAnInteger
 
 @login_required
 @staff_required
@@ -15,11 +17,39 @@ def sales(request):
     if start_date and end_date:
         walk_in_purchases = walk_in_purchases.filter(created_at__date__range=[start_date, end_date])
 
-    walk_in_purchase_items = PurchaseItem.objects.filter(purchase__in=walk_in_purchases).order_by('-created_at')
+    walk_in_purchase_items_list = PurchaseItem.objects.filter(purchase__in=walk_in_purchases).order_by('-created_at')
+    checked_out_rooms_list = Customer.objects.filter(is_active=False).order_by('-check_in_date')
 
-    checked_out_rooms = Customer.objects.filter(is_active=False).order_by('-check_in_date')
     if start_date and end_date:
-        checked_out_rooms = checked_out_rooms.filter(check_out_date__date__range=[start_date, end_date])
+        checked_out_rooms_list = checked_out_rooms_list.filter(check_out_date__date__range=[start_date, end_date])
+
+    items_per_page = 5
+    
+    walk_in_purchases_page = request.GET.get('walk_in_page', 1)
+    checked_out_rooms_page = request.GET.get('rooms_page', 1)
+
+    walk_in_purchases_paginator = Paginator(walk_in_purchase_items_list, items_per_page)
+    checked_out_rooms_paginator = Paginator(checked_out_rooms_list, items_per_page)
+
+    try:
+        walk_in_purchase_items = walk_in_purchases_paginator.page(walk_in_purchases_page)
+    except PageNotAnInteger:
+        walk_in_purchase_items = walk_in_purchases_paginator.page(1)
+    except EmptyPage:
+        walk_in_purchase_items = walk_in_purchases_paginator.page(walk_in_purchases_paginator.num_pages)
+
+    try:
+        checked_out_rooms = checked_out_rooms_paginator.page(checked_out_rooms_page)
+    except PageNotAnInteger:
+        checked_out_rooms = checked_out_rooms_paginator.page(1)
+    except EmptyPage:
+        checked_out_rooms = checked_out_rooms_paginator.page(checked_out_rooms_paginator.num_pages)
+
+    room_total_sales = 0
+    walk_in_total_sales = 0
+
+    for item in walk_in_purchase_items_list:
+        walk_in_total_sales += item.price_at_purchase
 
     for room in checked_out_rooms:
         total_extra_items = 0
@@ -40,6 +70,26 @@ def sales(request):
 
         room.subtotal = f'₱&nbsp;{subtotal:,.2f}'
 
+    for room in checked_out_rooms_list:
+        total_extra_items = 0
+        total_extra_items_subtotal = 0
+        purchase = Purchase.objects.filter(customer=room).first()
+        if purchase:
+            purchase_items = PurchaseItem.objects.filter(purchase=purchase)
+            for item in purchase_items:
+                total_extra_items += item.quantity
+                total_extra_items_subtotal += item.price_at_purchase * item.quantity
+
+        room.total_extra_items = total_extra_items
+        room.total_extra_items_subtotal = total_extra_items_subtotal
+
+        total_extra_bed_subtotal = room.extra_bed * room.extra_bed_price_at_check_in
+
+        subtotal = room.price_at_check_in + room.total_extra_items_subtotal + total_extra_bed_subtotal
+
+        room.subtotal = f'₱&nbsp;{subtotal:,.2f}'
+        room_total_sales += subtotal
+
     breadcrumbs = [
         ('Dashboard', '/dashboard/'),
         ('Sales', None), 
@@ -50,6 +100,12 @@ def sales(request):
         'walk_in_purchases': walk_in_purchases,
         'walk_in_purchase_items': walk_in_purchase_items,
         'checked_out_rooms': checked_out_rooms,
+        'room_total_sales': f'₱&nbsp;{room_total_sales:,.2f}',
+        'walk_in_total_sales': f'₱&nbsp;{walk_in_total_sales:,.2f}',
+        'start_date': start_date,
+        'end_date': end_date,
+        'walk_in_purchases_page': walk_in_purchases_page,
+        'checked_out_rooms_page': checked_out_rooms_page,
     }
 
     return render(request, 'sales.html', context)
